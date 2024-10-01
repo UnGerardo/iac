e75adfc051821c7c63f65731bc03888c4f9b8d0e
UnGerardo <gerardojdls@gmail.com>
2024-09-30 17:27:18 -0700

    - Add simple html file to bucket

--- provision-infra-s3/__main__.py
+++ provision-infra-s3/__main__.py
@@ -1,6 +1,12 @@
-"""A Python Pulumi program"""
-
 import pulumi
 import pulumi_aws as aws
+import os
 
 bucket = aws.s3.Bucket('my-bucket')
+filepath = os.path.join('website', 'index.html')
+obj = aws.s3.BucketObject('index.html',
+  bucket=bucket,
+  source=pulumi.FileAsset(filepath)
+)
+
+pulumi.export('bucket_name', bucket.bucket)
