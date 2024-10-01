24131bb300e1f0212049efe05f75a4cf8334d184
UnGerardo <gerardojdls@gmail.com>
2024-09-30 19:28:43 -0700

    - Host website from s3 bucket

--- provision-infra-s3/__main__.py
+++ provision-infra-s3/__main__.py
@@ -1,12 +1,49 @@
 import pulumi
 import pulumi_aws as aws
 import os
+import mimetypes
 
-bucket = aws.s3.Bucket('my-bucket')
-filepath = os.path.join('website', 'index.html')
-obj = aws.s3.BucketObject('index.html',
-  bucket=bucket,
-  source=pulumi.FileAsset(filepath)
+bucket = aws.s3.BucketV2('web-bucket')
+
+bucketWebConfig = aws.s3.BucketWebsiteConfigurationV2('s3-website',
+  bucket=bucket.bucket,
+  index_document={
+    'suffix': 'index.html'
+  }
 )
 
-pulumi.export('bucket_name', bucket.bucket)
+bucketPublicAccessBlock = aws.s3.BucketPublicAccessBlock('public-access-block',
+  bucket=bucket.bucket,
+  block_public_acls=False
+)
+
+contentDir = 'website'
+for file in os.listdir(contentDir):
+  filePath = os.path.join(contentDir, file)
+  mimetype, _ = mimetypes.guess_type(filePath)
+  obj = aws.s3.BucketObject(file,
+    bucket=bucket.id,
+    source=pulumi.FileAsset(filePath),
+    content_type=mimetype
+  )
+
+bucketPolicy = aws.s3.BucketPolicy('bucket-policy',
+  bucket=bucket.id,
+  opts=pulumi.ResourceOptions(depends_on=[bucketPublicAccessBlock]),
+  policy=pulumi.Output.json_dumps({
+    'Version': '2012-10-17',
+    'Statement': [
+      {
+        'Effect': 'Allow',
+        'Principal': '*',
+        'Action': ['s3:GetObject'],
+        'Resource': [
+          pulumi.Output.format('arn:aws:s3:::{0}/*', bucket.id)
+        ]
+      }
+    ]
+  })
+)
+
+pulumi.export('bucket_name', bucket.id)
+pulumi.export('website_url', pulumi.Output.concat('http://', bucketWebConfig.website_endpoint))
