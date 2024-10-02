15d013c407d3a81107bd30d21e7e0968adb36163
UnGerardo <gerardojdls@gmail.com>
2024-10-02 15:24:20 -0700

    - Create a loadbalancer and add all instances

--- prov-ec2/__main__.py
+++ prov-ec2/__main__.py
@@ -13,10 +13,33 @@ group = aws.ec2.SecurityGroup(
   ingress=[
       { 'protocol': 'icmp', 'from_port': 8, 'to_port': 0, 'cidr_blocks': ['0.0.0.0/0'] },
       { 'protocol': 'tcp', 'from_port': 80, 'to_port': 80, 'cidr_blocks': ['0.0.0.0/0'] },
+  ],
+  egress=[
+    { 'protocol': 'tcp', 'from_port': 80, 'to_port': 80, 'cidr_blocks': ['0.0.0.0/0'] },
   ]
 )
 
 vpc = aws.ec2.get_vpc(default=True)
+vpcSubnets = aws.ec2.get_subnets(filters=[{"name": "vpc-id", "values": [vpc.id]}])
+
+loadBalancer = aws.lb.LoadBalancer(
+  'loadbalancer',
+  internal=False,
+  security_groups=[group.id],
+  subnets=vpcSubnets.ids,
+  load_balancer_type='application'
+)
+
+targetGroup = aws.lb.TargetGroup('target-group', port=80, protocol='HTTP', target_type='ip', vpc_id=vpc.id)
+
+listener = aws.lb.Listener("listener",
+  load_balancer_arn=loadBalancer.arn,
+  port=80,
+  default_actions=[{
+    "type": "forward",
+    "target_group_arn": targetGroup.arn
+  }]
+)
 
 ips = []
 hostnames = []
@@ -41,5 +64,13 @@ for az in aws.get_availability_zones().names:
     ips.append(server.public_ip)
     hostnames.append(server.public_dns)
 
+    attachmennt = aws.lb.TargetGroupAttachment(
+      f'web-server-{az}',
+      target_group_arn=targetGroup.arn,
+      target_id=server.private_ip,
+      port=80
+    )
+
 pulumi.export('ips', ips)
 pulumi.export('hostnames', hostnames)
+pulumi.export('url', loadBalancer.dns_name)
