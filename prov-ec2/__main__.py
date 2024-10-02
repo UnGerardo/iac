632cfe4bab11c3e69444834547e7c9040a6a8560
UnGerardo <gerardojdls@gmail.com>
2024-10-02 13:39:06 -0700

    - Create an instance in each availability zone

--- prov-ec2/__main__.py
+++ prov-ec2/__main__.py
@@ -18,19 +18,28 @@ group = aws.ec2.SecurityGroup(
 
 vpc = aws.ec2.get_vpc(default=True)
 
-server = aws.ec2.Instance(
-  'web-server',
-  instance_type="t2.micro",
-  vpc_security_group_ids=[group.id],
-  ami=ami.id,
-  user_data="""#!/bin/bash
-    echo \"Hello, World!\" > index.html
-    nohup python -m SimpleHTTPServer 80 &
-    """,
-  tags={
-      "Name": "web-server",
-  },
-)
+ips = []
+hostnames = []
 
-pulumi.export('ip', server.public_ip)
-pulumi.export('hostname', server.public_dns)
+for az in aws.get_availability_zones().names:
+  if not az == 'us-west-2d':
+    server = aws.ec2.Instance(
+      f'web-server-{az}',
+      instance_type="t2.micro",
+      vpc_security_group_ids=[group.id],
+      ami=ami.id,
+      availability_zone=az,
+      user_data="""#!/bin/bash
+        echo \"Hello, World! -- from {}\" > index.html
+        nohup python -m SimpleHTTPServer 80 &
+        """.format(az),
+      tags={
+          "Name": "web-server",
+      },
+    )
+
+    ips.append(server.public_ip)
+    hostnames.append(server.public_dns)
+
+pulumi.export('ips', ips)
+pulumi.export('hostnames', hostnames)
